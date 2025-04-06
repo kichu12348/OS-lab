fe72419d5bdab9850ea1182653daac66e4f1b2eb
Mahadevan Reji <rmahadevan574@gmail.com>
2025-04-06 16:38:30 +0530

    Refactor deadlock detection algorithm for improved readability and consistency

--- Dead Lock/deadLockDetect.c
+++ Dead Lock/deadLockDetect.c
@@ -1,6 +1,4 @@
-#include<stdio.h>
-
-
+#include <stdio.h>
 
 /*
 Deadlock Detection Algorithm:
@@ -30,69 +28,86 @@ void detection(
     int p[][10],
     int req[][10],
     int n,
-    int m
-){
+    int m)
+{
     int work[m];
-    for(int i=0;i<m;i++) work[i]=avail[i];
+    for (int i = 0; i < m; i++)
+        work[i] = avail[i];
 
-    int count=0;
+    int count = 0;
     int finish[n];
-    for(int i=0;i<n;i++) finish[i]=0;
+    for (int i = 0; i < n; i++)
+        finish[i] = 0;
 
-    while(count <n){
-        int found=0;
-        for(int i=0;i<n;i++){
-            int j;
-            for(j=0;j<m;j++) 
-                if(req[i][j]>work[j]) break;
+    while (count < n)
+    {
+        int found = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!finish[i])
+            {
+                int j;
+                for (j = 0; j < m; j++)
+                    if (req[i][j] > work[j])
+                        break;
 
-            if(j==m){
-                for(int k=0;k<m;k++) work[k]+=p[i][k];
-                finish[i]=1;
-                found=1;
+                if (j == m)
+                {
+                    for (int k = 0; k < m; k++)
+                        work[k] += p[i][k];
+                    finish[i] = 1;
+                    found = 1;
+                    count++;
+                }
             }
         }
 
-        if(!found) return;
+        if (!found)
+            break;
     }
 
-    int deadlock=0;
+    int deadlock = 0;
     printf("Processes in deadlock: ");
-    for(int i=0;i<n;i++){
-        if(!finish[i]){
-            deadlock=1;
-            printf("P%d ",i);
+    for (int i = 0; i < n; i++)
+    {
+        if (!finish[i])
+        {
+            deadlock = 1;
+            printf("P%d ", i);
         }
     }
     printf("\n");
-    if(deadlock){
+    if (deadlock)
+    {
         printf("System is not safe deadlock detected\n");
-
     }
-    else printf("No deadlock detected\n");
+    else
+        printf("No deadlock detected\n");
 }
 
-void main(){
-    int n,m;
+void main()
+{
+    int n, m;
     printf("Enter the number of processes: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("Enter the number of resources: ");
-    scanf("%d",&m);
+    scanf("%d", &m);
 
     int avail[m];
     printf("Enter the available resources: ");
-    for(int i=0;i<m;i++) scanf("%d",&avail[i]);
+    for (int i = 0; i < m; i++)
+        scanf("%d", &avail[i]);
 
-    int p[n][10],req[n][10];
+    int p[n][10], req[n][10];
     printf("Enter the allocation matrix:\n");
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            scanf("%d",&p[i][j]);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &p[i][j]);
 
     printf("Enter the request matrix:\n");
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            scanf("%d",&req[i][j]);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &req[i][j]);
 
-    detection(avail,p,req,n,m);
+    detection(avail, p, req, n, m);
 }
